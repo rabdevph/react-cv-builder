a9bf5b4deb4412799884c01b843421c804b642de
Ralph Arsy Bauto <rab.devph@gmail.com>
2023-07-24 14:18:29 +0400

    Add left and right panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { LeftPanel } from './panels/LeftPanel/LeftPanel.jsx';
+import { RightPanel } from './panels/RightPanel/RightPanel.jsx';
+
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="app">
+      <LeftPanel />
+      <RightPanel />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/panels/LeftPanel/LeftPanel.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './leftpanel.css';
+
+export const LeftPanel = () => {
+  return <div className="left-panel">Forms</div>;
+};
